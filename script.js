a7f28c76e0e48d55c6f51de11da468ee73095693
Javier Lona <javierlona1@gmail.com>
2018-07-08 16:02:20 -0500

    required working

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-// document.theform.onsubmit = function() {
-//   alert('submission interrupted');
-//   return false;
-// }
-
 const THEFORM = document.querySelector('#contact-form');
-// console.log(THEFORM);
 
 THEFORM.addEventListener('submit', check_form);
+
+var myError = document.querySelector('.alert.alert-danger');
+console.log(myError);
+
+var validationInfo = {
+  "name" : {
+    "required" : true
+  },
+  "email" : {
+    "required" : true
+  },
+  "phone" : {
+    "required" : true,
+    "pattern" : "\\d{3}[\\-]\\d{3}[\\-]\\d{4}"
+  }
+};
 
 function check_form(event) {
   event.preventDefault();
   console.log("Something");
+
+  for (key in validationInfo) {
+    var myField = document.getElementById(key);
+    console.log(myField);
+    if((validationInfo[key].required) && (myField.value == '')) {
+      myError.innerHTML = "Required field " + key + " not filled.";
+      myField.select();
+      return false;
+    }
+  }
+  return true;
 }
