5eb27f51e7788f277f2bd502ed66f56cade378f3
Javier Lona <javierlona1@gmail.com>
2018-07-08 20:08:41 -0500

    got the warning danger working

--- script.js
+++ script.js
@@ -2,56 +2,53 @@ const THEFORM = document.querySelector('#contact-form');
 
 THEFORM.addEventListener('submit', check_form);
 
-var myError = document.querySelector('.alert.alert-danger');
-// console.log(myError);
-
 var validationInfo = {
   "name" : {
-    "required" : false
+    "required" : true
   },
   "email" : {
-    "required" : false
+    "required" : true
   },
   "phone" : {
-    "required" : true,
-    "pattern" : "\\d{3}[\\-]\\d{3}[\\-]\\d{4}"
+    "required" : true
   },
 };
 
+var newDiv = document.createElement('div');
+newDiv.className = 'alert alert-danger d-none';
+THEFORM.append(newDiv);
+THEFORM.insertBefore(newDiv, THEFORM.firstElementChild);
+
 function check_form(event) {
   event.preventDefault();
-  console.log("Something");
-
   for (key in validationInfo) {
     var myField = document.getElementById(key);
-    // var myPattern = validationInfo[key].pattern;
-    // console.log("pattern in first ",myPattern);
     if((validationInfo[key].required) && (myField.value == '')) {
-      myError.innerHTML = "Required field " + key + " not filled.";
+      newDiv.classList.remove("d-none");
+      newDiv.innerHTML = "Required field " + key + " not filled.";
       myField.select();
       return false;
     }
   }
+  newDiv.innerHTML = "";
+  newDiv.classList.add("d-none");
   return true;
 }
 
 // PATTERNS
 var inputFields = THEFORM.querySelectorAll("input");
-console.log(inputFields);
 
 for (key in inputFields) {
   var theFields = inputFields[key];
   theFields.onchange = function() {
     var myPattern = this.pattern;
-
-    // console.log("pattern in second ",myPattern);
     var myPlaceholder = this.placeholder;
     var isValid = this.value.search(myPattern) >= 0;
-    console.log(isValid);
     if(!(isValid)) {
-      myError.innerHTML = "Input does not match expected pattern." + myPlaceholder;
+      newDiv.innerHTML = "Input does not match expected pattern." + myPlaceholder;
     } else {
-      myError.innerHTML = "";
+      newDiv.innerHTML = "";
+      newDiv.classList.add("d-none");
     }
   }
 }
